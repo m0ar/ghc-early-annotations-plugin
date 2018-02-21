02a2b7327d0bb20bee00ac6c4b3a2c3e1f3f1dda
Edvard Hübinette <edvard@hubinette.me>
2018-02-21 16:24:18 +0100

    Implement findAnnDecls and document EarlyAnnPlugin

--- EarlyAnnPlugin/src/EarlyAnnPlugin.hs
+++ EarlyAnnPlugin/src/EarlyAnnPlugin.hs
@@ -1,12 +1,44 @@
 module EarlyAnnPlugin (plugin) where
 
 import GhcPlugins
+import HsDecls (HsDecl(..), AnnDecl(..), AnnProvenance(..))
+import HsSyn (hsmodDecls)
+import HsExpr (HsExpr(..))
+import HsExtension (IdP(..), GhcPs)
 
 plugin :: Plugin
 plugin = defaultPlugin {
     parsedResultAction = prepareWeights
   }
 
+-- | Extracts binders with payload from 'ANN' pragmas through 'HsParsedModule'
+-- and adds them to the typechecking global environment 'TcGblEnv', for use
+-- later in the compilation pipeline.
+--
+-- Note: if TcGblEnv is deemed unfit for this job, we need to find another way
+-- to stash the 'ANN' binders and associated payload.
 prepareWeights :: [CommandLineOption] -> ModSummary -> HsParsedModule
                    -> Hsc HsParsedModule
 prepareWeights _ _ = return
+
+type StrippedAnnD = (AnnProvenance RdrName, HsExpr GhcPs)
+
+-- | Traverses the top level declarations in the module, finds annotations
+-- and returns the annotated binding together with the payload expression.
+findAnnDecls :: HsParsedModule -> [StrippedAnnD]
+findAnnDecls hpm = let L _ hsModule = hpm_module hpm in
+  let lHsDecls = hsmodDecls hsModule in stripAndFilter lHsDecls
+
+-- | Strips location wrappers and collects content from ANN declarations from
+-- the top level declarations; we do not traverse deeper into the AST.
+--
+-- HsDecl has a constructor 'AnnD (AnnDecl name)', where
+-- 'AnnDecl name = HsAnnotation (AnnProvenance name) (Located (HsExpr name))'
+stripAndFilter :: [Located (HsDecl GhcPs)] -> [StrippedAnnD]
+stripAndFilter = foldr unwrapAnnD []
+  where 
+    unwrapAnnD :: Located (HsDecl GhcPs) -> [StrippedAnnD] -> [StrippedAnnD]
+    unwrapAnnD (L _ (AnnD (HsAnnotation _ annProv lHsExpr))) annDecls =
+      let L _ hsExpr = lHsExpr in (annProv, hsExpr):annDecls
+    unwrapAnnD _ annDecls = annDecls
+    -- ^ Not interested in anything else, so we skip any other declaration
